90e335a1cc009d02356785350780d6ebe73f3665
prashant447 <119475292+prashant447@users.noreply.github.com>
2023-12-03 17:44:42 +0530

    first commit

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,62 +1,57 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { IoMdArrowDropdown } from "react-icons/io";
 import "./Header.css";
 
-
 const Header = () => {
+  const [isShow, setIsShow] = useState(false);
 
-  const [isShow, setIsShow] = useState(false)
-
-  const showMenu = () =>{
-       if (isShow == true) {
-        setIsShow(false)
-       } else {
-        setIsShow(true)
-       }
-  }
+  const showMenu = () => {
+    if (isShow == true) {
+      setIsShow(false);
+    } else {
+      setIsShow(true);
+    }
+  };
   return (
     <>
-     <header>
+      <header>
         <nav>
-        <div className="logo">
-           <h2>AIPatrn</h2>
-       </div>
-         
-         {isShow && <> <div className="menu">
-        <ul>
-            <Link to= "#">Home page</Link>
-            <Link to= "#">Generator</Link>
-            <Link to= "#">Pricing-plan</Link>
-            <Link to= "#">Blog-News</Link>
-        </ul>
+          <div className="logo">
+            <h2>AIPatrn</h2>
+          </div>
 
-        <ul>
-            <Link to= "#">About us</Link>
-            <Link to= "#">Features</Link>
-            <Link to= "#">Collection-2023</Link>
-            <Link to= "#">Career</Link>
-        </ul>
-       </div>
-         </>}
-       
+          {isShow && (
+            <>
+              
+              <div className="menu">
+                <ul>
+                  <Link to="#">Home page</Link>
+                  <Link to="#">Generator</Link>
+                  <Link to="#">Pricing-plan</Link>
+                  <Link to="#">Blog-News</Link>
+                </ul>
 
-       <div className='menu-btns'>
-        <Link className='menu-btn' onClick={showMenu}>Menu<IoMdArrowDropdown className='svg1'/></Link>
+                <ul>
+                  <Link to="#">About us</Link>
+                  <Link to="#">Features</Link>
+                  <Link to="#">Collection-2023</Link>
+                  <Link to="#">Career</Link>
+                </ul>
+              </div>
+            </>
+          )}
 
-       </div>
-
-
-      
+          <div className="menu-btns">
+            <Link className="menu-btn" onClick={showMenu}>
+              Menu
+              <IoMdArrowDropdown className="svg1" />
+            </Link>
+          </div>
         </nav>
-       
-        
-     </header>
-
-
-      
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,82 +1,97 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 
 import { HiMiniArrowDownRight } from "react-icons/hi2";
-import img1 from "/src/assets/man.jpg"
-import img2 from "/src/assets/mans.jpg"
-import img3 from "/src/assets/woman.jpg"
-
+import img1 from "/src/assets/man.jpg";
+import img2 from "/src/assets/mans.jpg";
+import img3 from "/src/assets/woman.jpg";
 
 const Home = () => {
   return (
     <>
       <main>
-
         <div className="main-section">
+          <div className="section1">
+            <div>
+              <p>Revolutionize Your </p>
+            </div>
+            <div>
+              <h1>
+                Creative Projects with <br /> the Ultimate AI-Powered{" "}
+              </h1>
+            </div>
 
-        
-        <div className="section1">
-           <div>
-            <p>Revolutionize Your </p>
-           </div>
-           <div>
-            <h1>Creative Projects with <br/> the Ultimate AI-Powered </h1>
-           </div>
-           
-
-           <div className='section2'>
-            <span className='heading'>Pattern </span>
-            <button>START GENERATING</button>
-            <p>The Next generation <br/> <b style={{color:"black"}}>Production</b> for Designers</p>
-           </div>
-        </div>
+            <div className="section2">
+              <span className="heading">Pattern </span>
+              <button>START GENERATING</button>
+              <p>
+                The Next generation <br />{" "}
+                <b style={{ color: "black" }}>Production</b> for Designers
+              </p>
+            </div>
+          </div>
         </div>
 
         <div className="section3">
+          <div className="grid1">
+            <span className="grid-number">01</span>
 
-            <div className="grid1">
-                <span className='grid-number'>01</span>
-                
-                <div className="avatar">
-                    <div  className='avatar-pic'>
+            <div className="avatar">
+              <div className="avatar-pic">
+                <img src={img1} alt="" />
+                <img
+                  src={img2}
+                  alt=""
+                  style={{ position: "relative", right: "9px" }}
+                />
+                <img
+                  src={img3}
+                  alt=""
+                  style={{ position: "relative", right: "20px" }}
+                />
+              </div>
 
-                    <img src= {img1} alt="" />
-                    <img src= {img2} alt="" style={{position:"relative", right:"9px"}}/>
-                    <img src= {img3} alt="" style={{position:"relative", right:"20px"}}/>
-                    </div>
-
-                    <h3>Learn from best mentors</h3>
-                    <button className='grid1-btn'>START LEARNING<HiMiniArrowDownRight className='gird1-icon'/></button>
-                </div>
-
+              <h3>Learn from best mentors</h3>
+              <button className="grid1-btn">
+                START LEARNING
+                <HiMiniArrowDownRight className="gird1-icon" />
+              </button>
             </div>
-            <div className="grid2">
-                <span className='grid-number'>02</span>
+          </div>
+          <div className="grid2">
+            <span className="grid-number">02</span>
 
-                 <div className='row2'>
-                <div className="grid2-row">Free Edit <HiMiniArrowDownRight className='grid2-icon'/></div>
-                <div className="grid2-row">Interactive <HiMiniArrowDownRight className='grid2-icon'/></div>
-                <div className="grid2-row">Easy interface <HiMiniArrowDownRight className='grid2-icon' /></div>
-                <div className="grid2-row">Compare to other <HiMiniArrowDownRight className='grid2-icon'/></div>
-                    </div>    
+            <div className="row2">
+              <div className="grid2-row">
+                Free Edit <HiMiniArrowDownRight className="grid2-icon" />
+              </div>
+              <div className="grid2-row">
+                Interactive <HiMiniArrowDownRight className="grid2-icon" />
+              </div>
+              <div className="grid2-row">
+                Easy interface <HiMiniArrowDownRight className="grid2-icon" />
+              </div>
+              <div className="grid2-row">
+                Compare to other <HiMiniArrowDownRight className="grid2-icon" />
+              </div>
             </div>
+          </div>
 
-            <div className="grid3">
-               <span className='grid-number'>03</span>
-               
-               <div className='row3'>
-                <h5 className='grid3-heading'>+20K</h5>
-                <p className='grid3-para'>Glass Pattern generated this<br/> week in the first release </p>
-               </div>
-               </div>
-            
+          <div className="grid3">
+            <span className="grid-number">03</span>
+
+            <div className="row3">
+              <h5 className="grid3-heading">+20K</h5>
+              <p className="grid3-para">
+                Glass Pattern generated this
+                <br /> week in the first release{" "}
+              </p>
+            </div>
+          </div>
         </div>
-
       </main>
-      
-      
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
